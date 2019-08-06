90afaee317f51eedca634adb146d42c7d2023db2
Arman DEJBAKHSH <adejbakh@e1r12p16.42.fr>
2019-08-06 16:44:05 +0200

    add reset mail function

--- model/helper/mail_helper.rb
+++ model/helper/mail_helper.rb
@@ -7,5 +7,11 @@ module MailHelper
             :subject => '=?UTF-8?B?Q29uZmlybSB5b3VyIG1hdGNoYSBhY2NvdW50IOKdpO+4jw==?=', 
             :body => "Hello there , click on the link to confirme your account and start finding love  http://matcha/?token=#{token}")
     end
-    def self.
+    def self.reset_mail(mail, token)
+        Pony.mail(
+            :to => mail, 
+            :from => 'noreply@matcha.fr', 
+            :subject => 'Did you forgot your Matcha password ?', 
+            :body => "Hello there , click on the link to reset your password account and start finding love again  http://matcha/?token=#{token}")
+        end
 end
