304df84c1fd8abdff486928db33838880cfabfff
Arman DEJBAKHSH <adejbakh@e1r4p20.42.fr>
2019-08-12 17:40:41 +0200

    removing binding pry and error on the forgot password controller

--- controller/helper/mail_helper.rb
+++ controller/helper/mail_helper.rb
@@ -9,7 +9,7 @@ module MailHelper
     end
     def reset_mail(mail, token)
         Pony.mail(
-            :to => mail, s
+            :to => mail,
             :from => 'noreply@matcha.fr', 
             :subject => 'Did you forgot your Matcha password ?', 
             :body => "Hello there , click on the link to login and reset your password -> start finding love again  http://localhost:4567/token/reset_password?token=#{token}")
--- controller/password_controller.rb
+++ controller/password_controller.rb
@@ -6,14 +6,14 @@ class PasswordController < ApplicationController
   end
 
   post '/forgot_password' do
-    settings.log.info(params)
-    binding.pry
     a = User.where(equality: {email: params[:email]})
     if a.any? && a[0].is_a?(User)
       rand_hash = rand(36**52).to_s(36)
       reset_mail(a[0].email, rand_hash)
-      a[0].email = rand_hash
+      a[0].reset_token = rand_hash
       a[0].save
     end
+    flash[:success] = "If you mail exist we sent you a mail"
+    redirect '/'
   end
 end
