629169ab01412f37306bb6bdc99c885ebfd43d1c
bafraiki <bafraiki@student.42.fr>
2019-08-22 19:06:03 +0200

    OMW to full notif

--- assets/javascript/notif.js
+++ assets/javascript/notif.js
@@ -1,9 +1,30 @@
 
-(function () {
-	const display_notif = function() {
-
+const display_notif = function(notif) {
+	const object = JSON.parse(notif.data);
+	switch (object.type) {
+		case 'SOMEONE_LIKED_YOU':
+			console.log("tu plais a quelqu'un petit coquin");
+			create_notif_like(object);
+			break;
+		case 'NEW_MATCH':
+			console.log("nouveau match");
+			break;
+		case 'NEW_MESSAGE':
+			console.log("nouveau message");
+			break;
+		default:
+			console.log("something strange happen", object.type);
 	}
+}
 
+const create_notif_like = function(notif) {
+	notif = create_notif(notif);
+	
+}
 
+const create_notif = function(notif) {
+	const body = document.createElement("p");
+	body.innerHTML = notif.type(/_/g, " ");
+	return (body);
+}
 
-})();
