3dd4512e371c0dd564959040253601a6068985fb
bafraiki <bafraiki@student.42.fr>
2019-08-24 20:35:32 +0200

    added like notif

--- assets/javascript/notif.js
+++ assets/javascript/notif.js
@@ -17,14 +17,44 @@ const display_notif = function(notif) {
 	}
 }
 
+const destroy_it = function() {
+	this.parentNode.removeChild(this);
+}
+
 const create_notif_like = function(notif) {
+	const wrapper = document.querySelector("#notif_wrapper");
+	const like_span = document.querySelector("#matcha_like span");
+	let number = parseInt(like_span.innerText);
+	if (!isNaN(number) && number <= 98)
+		like_span.innerText = ++number;
+	like_span.parentNode.classList.add("active");
 	notif = create_notif(notif);
-	
+	wrapper.append(notif);
+}
+
+const tell_this_notif_what_should_remove_it = function(notif) {
+	notif.addEventListener("remove", destroy_it, {once: true});
+	let timer = setTimeout(function(notif) {notif.dispatchEvent(new Event("remove"))}, 3000, notif);
+	notif.addEventListener("click", function(notif) {
+		notif.dispatchEvent(new Event("remove"));
+	}.bind(this, notif));
 }
 
 const create_notif = function(notif) {
 	const body = document.createElement("p");
-	body.innerHTML = notif.type(/_/g, " ");
+	body.classList.add("badge");
+	body.classList.add("badge-info");
+	body.innerHTML = notif.type.replace(/_/g, " ");
+	tell_this_notif_what_should_remove_it(body);
 	return (body);
 }
 
+
+window.onload = () => {
+	document.querySelector("#matcha_like").onclick = function() {
+		this.classList.remove("active");}
+	document.querySelector("#matcha_match").onclick = function() {
+		this.classList.remove("active");}
+	document.querySelector("#matcha_conv").onclick = function() {
+		this.classList.remove("active");}
+};
