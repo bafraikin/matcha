b0116fd7c21fa4c5310e9636fe73fabb187fc0a2
Arman DEJBAKHSH <adejbakh@e1r4p5.42.fr>
2019-09-27 15:42:01 +0200

    if user doesn't have a localisation -> default localisation is Paris

--- controller/helper/geolocalisation_helper.rb
+++ controller/helper/geolocalisation_helper.rb
@@ -2,7 +2,10 @@ module GeolocalisationHelper
     def save_if_valide_coordinate(latitude, longitude)
         latitude = latitude.to_f
         longitude = longitude.to_f
-        return "geolocalisation error" if (latitude == 0 || longitude == 0)
+        if (latitude == 0 || longitude == 0)
+            latitude = 48.9026985168457
+            longitude = 2.304080009460449
+        end
         current_user.latitude = latitude
         current_user.longitude = longitude
         current_user.save
